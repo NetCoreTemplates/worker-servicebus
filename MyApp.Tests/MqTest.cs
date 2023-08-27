afd1bc02ca29ffa8d076e40c18124cf8bf93a896
Demis Bellot <demis.bellot@gmail.com>
2023-08-28 00:00:27 +0800

    Upgrade to latest v6.10.1 GenericAppHost

--- MyApp.Tests/MqTest.cs
+++ MyApp.Tests/MqTest.cs
@@ -1,39 +1,35 @@
-using System.Threading.Tasks;
 using MyApp.ServiceModel;
 using NUnit.Framework;
 using ServiceStack.Messaging;
 using ServiceStack.Azure.Messaging;
 
-namespace MyApp.Tests
+namespace MyApp.Tests;
+
+public class MqTest
 {
-    public class MqTest
+    private ServiceBusMqServer MqServer;
+    private readonly IMessageFactory MqFactory;
+    public MqTest()
     {
-        private ServiceBusMqServer MqServer;
-        private readonly IMessageFactory MqFactory;
-        public MqTest()
+        MqServer = new ServiceBusMqServer("<ServiceBus ConnectionString>") {
+            DisablePublishingToOutq = true,
+        };
+        MqFactory = MqServer.MessageFactory;
+    }
+
+    [Test] // requires running Host MQ Server project
+    public void Can_send_Request_Reply_message()
+    {
+        using var mqClient = MqFactory.CreateMessageQueueClient();
+        var replyToMq = mqClient.GetTempQueueName();
+
+        mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" })
         {
-            MqServer = new ServiceBusMqServer("<ServiceBus ConnectionString>") {
-                DisablePublishingToOutq = true,
-            };
-            MqFactory = MqServer.MessageFactory;
-        }
+            ReplyTo = replyToMq,
+        });
 
-        [Test] // requires running Host MQ Server project
-        public void Can_send_Request_Reply_message()
-        {
-            using (var mqClient = MqFactory.CreateMessageQueueClient())
-            {
-                var replyToMq = mqClient.GetTempQueueName();
-
-                mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" })
-                {
-                    ReplyTo = replyToMq,
-                });
-
-                var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
-                mqClient.Ack(responseMsg);
-                Assert.That(responseMsg.GetBody().Result, Is.EqualTo("Hello, MQ Worker!"));
-            }
-        }
+        var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
+        mqClient.Ack(responseMsg);
+        Assert.That(responseMsg.GetBody().Result, Is.EqualTo("Hello, MQ Worker!"));
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,40 +1,35 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using ServiceStack;
 using ServiceStack.Messaging;
 using ServiceStack.Azure.Messaging;
 using MyApp.ServiceModel;
 
-namespace MyApp
+namespace MyApp;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static ServiceStackHost? AppHost { get; set; }
-
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+        CreateHostBuilder(args)
+            .Build()
+            .UseServiceStack(new GenericAppHost(typeof(MyService).Assembly)
+            {
+                ConfigureAppHost = host =>
                 {
-                    AppHost = new GenericAppHost(typeof(MyService).Assembly)
-                    {
-                        ConfigureAppHost = host =>
-                        {
-                            var mqServer = new ServiceBusMqServer(hostContext.Configuration.GetConnectionString("ServiceBus")) {
-                                DisablePublishingToOutq = true,
-                            };
-                            mqServer.RegisterHandler<Hello>(host.ExecuteMessage);
-                            host.Register<IMessageService>(mqServer);
-                        }
-                    }.Init();
-
-                    services.AddSingleton(AppHost.Resolve<IMessageService>());
-                    services.AddHostedService<MqWorker>();
-                });
+                    var mqServer = host.Resolve<IMessageService>();
+                    mqServer.RegisterHandler<Hello>(host.ExecuteMessage);
+                }
+            })
+            .Run();
     }
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<IMessageService>(new ServiceBusMqServer(
+                    hostContext.Configuration.GetConnectionString("ServiceBus")) {
+                    DisablePublishingToOutq = true,
+                });
+                services.AddHostedService<MqWorker>();
+            });
 }
